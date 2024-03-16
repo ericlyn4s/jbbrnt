19a6595d88ef13b4c4efd56b94996049911a240b
Eric Peterson <ericmpeterson04@gmail.com>
2024-03-16 13:30:41 -0600

    Got connection to work and created database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://172.0.0.1:27017/jbbrntDB';
+const connectionString = 'mongodb://127.0.0.1:27017/jbbrntDB';
 
 connect(connectionString);
 
--- index.js
+++ index.js
@@ -9,8 +9,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
-  });
-});
+console.log(`App is running at ${PORT}!`);
