85df7ffa3ac54b19205e68575f75eb5961086fa2
Eric Peterson <ericmpeterson04@gmail.com>
2024-03-17 16:16:12 -0600

    Small adjustment to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -32,7 +32,7 @@ module.exports = {
     try {
       const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
-        { $push: { thoughts: thoughtId} },
+        { $push: { thoughts: req.params.thoughtId } },
         { runValidators: true, new: true }
       );
 
