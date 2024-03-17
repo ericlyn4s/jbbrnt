0d16efc31f3d0d341451ce1ec45ca342f0f363a2
Eric Peterson <ericmpeterson04@gmail.com>
2024-03-17 14:58:30 -0600

    Got friend add function to work:

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,8 +15,8 @@ router.route('/').get(getUsers).post(createUser);
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
-// /api/users/:userId/friends
-router.route('/:userId/friends').post(addFriend);
+// /api/users/:userId/friends/:friendID
+router.route('/:userId/friends/:friendId').post(addFriend);
 
 // /api/users/:userId/friends/:userId
 router.route('/:userId/friends/:userId').delete(removeFriend);
